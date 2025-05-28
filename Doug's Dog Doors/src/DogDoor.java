4fbb9cf6b3b7f886a93fc63453c57ce8bdb1c87c
kajal01 <warkhadekajal@gmail.com>
2025-05-28 16:51:33 +0530

    Timer added to DogDoor ,instead of Remote , to automatically close the door

--- Doug's Dog Doors/src/DogDoor.java
+++ Doug's Dog Doors/src/DogDoor.java
@@ -1,3 +1,6 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoor
 {
     private boolean open;
@@ -11,6 +14,17 @@ public class DogDoor
     {
         System.out.println("The dog door opens.");
         open = true;
+
+        final Timer timer = new Timer();
+            timer.schedule(
+                    new TimerTask()
+                    {
+                        public void run()
+                        {
+                            close();
+                            timer.cancel();
+                        }
+                    },5000);
     }
 
     public void close()
